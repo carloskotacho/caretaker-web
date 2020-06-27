5cab4fa5ff52038b9b025da4f77667910eb49aba
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-27 00:10:27 -0300

    Added: form to repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { RepositoryModule } from './repository/repository.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     BrowserAnimationsModule,
 
-    CoreModule
+    CoreModule,
+    RepositoryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/repository/repository.module.ts
+++ src/app/repository/repository.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
+import { MainComponent } from './main/main.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+
+    InputTextModule,
+    ButtonModule
+  ],
+  declarations: [
+    MainComponent,
+    RepositoryComponent
+  ],
+  exports: [
+    MainComponent,
+    RepositoryComponent
+  ],
+  providers: []
 })
 export class RepositoryModule { }
