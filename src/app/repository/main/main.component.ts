fb5be849825610d4dc6d92e4f363d25a3083388d
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-29 16:06:37 -0300

    Added: tests table for repositories

--- src/app/repository/main/main.component.ts
+++ src/app/repository/main/main.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  repositories = [
+    { name: 'angular/components',
+      description: 'Component infrastructure and Material Design components for Angular ' },
+    { name: 'spring-projects/spring-boot',
+      description: 'Spring Boot' },
+    { name: 'facebook/react',
+      description: 'A declarative, efficient, and flexible JavaScript library for building user interfaces.' },
+  ];
 
 }
