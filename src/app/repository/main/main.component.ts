6f1520d7b4219f6125921edd4f05102a8f7ef2a1
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-30 17:04:40 -0300

    data return

--- src/app/repository/main/main.component.ts
+++ src/app/repository/main/main.component.ts
@@ -9,9 +9,6 @@ import { RepositoryFilter, RepositoryService } from '../repository.service';
 export class MainComponent implements OnInit {
 
   filter = new RepositoryFilter();
-  avatarUrl = "undefined";
-  fullName = "undefined";
-  description = "undefined";
   repositories = [];
 
   constructor(private repositoryService: RepositoryService) {}
@@ -23,14 +20,10 @@ export class MainComponent implements OnInit {
   search() {
     this.repositoryService.search(this.filter)
       .then(repository => {
-        this.avatarUrl = repository.owner.avatar_url;
-        this.fullName = repository.full_name;
-        this.description = repository.description;
+        this.repositories.push(repository);
 
-        console.log('fullName: ', this.fullName);
-
-        this.repositories.push(this.fullName);
-
+        console.log(this.repositories);
+        console.log('full name: ', this.repositories['full_name']);
       });
   }
 
